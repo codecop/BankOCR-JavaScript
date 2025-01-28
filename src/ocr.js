c170a5fe46bd0080d124ca117e8f9a187e37444f
Peter Kofler <peter.kofler@code-cop.org>
2025-01-28 13:28:23 +0100

    Rename the hell out of it.

--- src/ocr.js
+++ src/ocr.js
@@ -43,39 +43,45 @@ const NUMERALS = [
 class Ocr {
 
     /**
-     * @param {string[]} lines 
+     * @param {string[]} inputLines 
      * @returns {string[]}
      */
-    static parse(lines) {
+    static parse(inputLines) {
         /** @type {string[]} */
-        const result = [];
-        for (let i = 0; i < lines.length; i += 4) {
-            let work = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-            for (let pos = 0; pos < 9; ++pos) {
-                work[pos] = '?';
-                let got1 = false;
-                for (let numeral = 0; numeral <= 9; ++numeral) {
-                    let ok = true;
+        const accountNumbers = [];
+
+        
+        for (let currentInputLine = 0; currentInputLine < inputLines.length; currentInputLine += 4) {
+            let accountNumber = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+            const numberOfAccountNumberDigits = 9;
+            for (let pos = 0; pos < numberOfAccountNumberDigits; ++pos) {
+                accountNumber[pos] = '?';
+                let foundMatchingDigit = false;
+                const availableDigits = 10;
+                for (let numeral = 0; numeral < availableDigits; ++numeral) {
+                    let isNumeralMatching = true;
                     for (let row = 0; row < 4; ++row) {
                         for (let col = 0; col < 4; ++col) {
-                            if (NUMERALS[numeral][row][col] !== lines[i + row][4 * pos + col])
-                                ok = false;
+                            if (NUMERALS[numeral][row][col] !== inputLines[currentInputLine + row][4 * pos + col]) {
+                                isNumeralMatching = false;
+                            }
                         }
                     }
-                    if (ok) {
-                        work[pos] = String.fromCharCode(numeral + '0'.charCodeAt(0));
-                        got1 = true;
+                    if (isNumeralMatching) {
+                        accountNumber[pos] = String.fromCharCode(numeral + '0'.charCodeAt(0));
+                        foundMatchingDigit = true;
                         break;
                     }
                 }
-                if (!got1) {
-                    work[10] = 'I';
-                    work[11] = work[12] = 'L';
+                if (!foundMatchingDigit) {
+                    accountNumber[10] = 'I';
+                    accountNumber[11] = accountNumber[12] = 'L';
                 }
             }
-            result.push(work.join(''));
+            accountNumbers.push(accountNumber.join(''));
         }
-        return result;
+
+        return accountNumbers;
     }
 }
 
